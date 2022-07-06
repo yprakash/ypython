67be46e676ca18437cf5af3f286819d6322984d2
yprakash <yprakash.518@gmail.com>
2022-07-06 10:30:23 +0530

    Fibonacci Using python Generator and yield

--- leetcode/easy-dp/fibonacci-number.py
+++ leetcode/easy-dp/fibonacci-number.py
@@ -1,8 +1,29 @@
 # @author: yprakash
 
 class Solution(object):
-    # https://leetcode.com/submissions/detail/739110272/
+    def fib_yield(self, n):
+        a, b = 0, 1
+
+        while n > 1:
+            n -= 1
+            a, b = b, a + b
+            yield a, b
+
+    # https://leetcode.com/submissions/detail/739707829/
+    # Using Generators and yield
     def fib(self, n):
+        if n <= 1:
+            return n
+        func = self.fib_yield(n)
+
+        while True:
+            try:
+                ans = next(func)
+            except StopIteration:
+                return ans[1]
+
+    # https://leetcode.com/submissions/detail/739110272/
+    def fib2(self, n):
         if n <= 1:
             return n
         a, b = 0, 1
@@ -13,5 +34,5 @@ class Solution(object):
 
 
 obj = Solution()
-for i in range(31):
+for i in range(10):
     print(obj.fib(i))
