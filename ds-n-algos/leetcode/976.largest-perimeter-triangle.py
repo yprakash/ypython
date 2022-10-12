ec9021a6cc6707813a7d696f287885d11d5c3194
yprakash <yprakash.518@gmail.com>
2022-10-12 08:41:47 +0530

    Largest Perimeter Triangle using Greedy& sorting

---
+++ ds-n-algos/leetcode/976.largest-perimeter-triangle.py
@@ -0,0 +1,14 @@
+# @author: yprakash
+from typing import List
+
+
+class Solution:
+    # https://leetcode.com/submissions/detail/820582547/
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+
+        for i in range(len(nums)-1, 1, -1):
+            if nums[i-2] + nums[i-1] > nums[i]:
+                return nums[i-2] + nums[i-1] + nums[i]
+
+        return 0
