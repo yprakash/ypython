f7a1e180ca9321c2e41007f51fbea4b70af6908d
yprakash <yprakash.518@gmail.com>
2022-10-22 00:12:30 +0530

    SortedDict (from sortedcontainers) based implementation

--- ds-n-algos/leetcode/2034.stock-price-fluctuation.py
+++ ds-n-algos/leetcode/2034.stock-price-fluctuation.py
@@ -1,5 +1,6 @@
 # @author: yprakash
 import heapq
+from sortedcontainers import SortedDict
 
 
 class StockPrice1:
@@ -34,3 +35,29 @@ class StockPrice1:
 
         heapq.heappush(self.min_heap, (price, ts))
         return price
+
+
+# https://leetcode.com/submissions/detail/827332622/
+class StockPrice2:
+    def __init__(self):  # O(1)
+        self.ts_price_map = SortedDict()
+        self.price_count_map = SortedDict()
+
+    def update(self, timestamp: int, price: int) -> None:  # O(logN)
+        if timestamp in self.ts_price_map:
+            old_price = self.ts_price_map[timestamp]
+            self.price_count_map[old_price] -= 1
+            if self.price_count_map[old_price] == 0:
+                del self.price_count_map[old_price]
+
+        self.ts_price_map[timestamp] = price
+        self.price_count_map[price] = self.price_count_map.get(price, 0) + 1
+
+    def current(self) -> int:  # O(logN)
+        return self.ts_price_map.peekitem(-1)[1]
+
+    def maximum(self) -> int:  # O(logN)
+        return self.price_count_map.peekitem(-1)[0]
+
+    def minimum(self) -> int:  # O(logN)
+        return self.price_count_map.peekitem(0)[0]
