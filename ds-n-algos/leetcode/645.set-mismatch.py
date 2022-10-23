ddc146d23d5167df45ace290b582323870db9020
yprakash <yprakash.518@gmail.com>
2022-10-23 08:33:49 +0530

    Repeating missing numbers using hashtable. ReDo using Bit Manipulation

---
+++ ds-n-algos/leetcode/645.set-mismatch.py
@@ -0,0 +1,18 @@
+# @author: yprakash
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        int_map = {}
+        repeating, missing = 0, 0
+
+        for n in nums:
+            if n in int_map:
+                repeating = n
+            else:
+                int_map[n] = True
+
+        for n in range(1, 1+len(nums)):
+            if n not in int_map:
+                return [repeating, n]
+
+        return [repeating, n]  # Shouldn't occur
