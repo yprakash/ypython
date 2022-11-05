a2579ca515e9259e7c83d679a3f2e8f69d578093
yprakash <yprakash.518@gmail.com>
2022-11-05 15:55:35 +0530

    Two Pointer approach

--- ds-n-algos/leetcode/28.find-the-index-of-the-first-occurrence-in-a-string.py
+++ ds-n-algos/leetcode/28.find-the-index-of-the-first-occurrence-in-a-string.py
@@ -4,3 +4,17 @@ class Solution:
     # https://leetcode.com/submissions/detail/837272607/
     def strStr(self, haystack: str, needle: str) -> int:
         return haystack.find(needle)
+
+    # https://leetcode.com/submissions/detail/837277601/
+    def strStr2(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack)):
+            found = True
+
+            for j, c in enumerate(needle):
+                if i+j >= len(haystack) or c != haystack[i + j]:
+                    found = False
+                    break
+            if found:
+                return i
+
+        return -1
