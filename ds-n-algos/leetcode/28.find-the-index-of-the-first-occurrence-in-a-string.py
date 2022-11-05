fce8ad06fbccd4313f6f3b9318c00645b0c7dd63
yprakash <yprakash.518@gmail.com>
2022-11-05 15:53:57 +0530

    one line string find index method

---
+++ ds-n-algos/leetcode/28.find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,6 @@
+# @author: yprakash
+
+class Solution:
+    # https://leetcode.com/submissions/detail/837272607/
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
