afaad8454fb002984e3c1e42e8544905fc16d29e
yprakash <yprakash.518@gmail.com>
2022-11-08 00:34:15 +0530

    Tabulation DP solution

--- ds-n-algos/leetcode/198.house-robber.py
+++ ds-n-algos/leetcode/198.house-robber.py
@@ -15,3 +15,16 @@ class Solution:
                        nums[index] + non_adjacent_max_sum(index+2))
 
         return non_adjacent_max_sum(0)
+
+    # https://leetcode.com/submissions/detail/838901586/
+    # Bottom-up Tabulation
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return max(nums)
+
+        dp = [0] * len(nums)
+        dp[0], dp[1] = nums[0], max(nums[1], nums[0])
+        for i in range(2, len(nums)):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+
+        return dp[-1]
