6904280a3c3bd4ac21939316b913ca0b576142f1
yprakash <yprakash.518@gmail.com>
2022-12-15 07:05:30 +0530

    LCS DP memoization recursive

---
+++ ds-n-algos/leetcode/1143.longest-common-subsequence.py
@@ -0,0 +1,15 @@
+# @author: yprakash
+from functools import cache
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        @cache
+        def longest(t1, t2):
+            if t1 < 0 or t2 < 0:
+                return 0
+            if text1[t1] == text2[t2]:
+                return 1 + longest(t1-1, t2-1)
+            return max(longest(t1-1, t2), longest(t1, t2-1))
+
+        return longest(len(text1) - 1, len(text2) - 1)
