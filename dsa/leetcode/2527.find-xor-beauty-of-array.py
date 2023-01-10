59b09f9a7f0429ee7c04ef0ce79fa93bc8a138d0
yprakash <yprakash.518@gmail.com>
2023-01-10 06:43:07 +0530

    functools reduce. Biweekly Contest 95

--- dsa/leetcode/2527.find-xor-beauty-of-array.py
+++ dsa/leetcode/2527.find-xor-beauty-of-array.py
@@ -3,6 +3,7 @@ from functools import reduce
 from typing import List
 
 
+# https://leetcode.com/contest/biweekly-contest-95/submissions/detail/873416385/
 class Solution:
     def xorBeauty(self, nums: List[int]) -> int:
         return reduce(lambda i, j: i ^ j, nums)
