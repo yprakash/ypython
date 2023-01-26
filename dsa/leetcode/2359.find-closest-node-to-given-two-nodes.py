6999d1578d85bfa49169e0620a41f29b3bc2818c
yprakash <yprakash.518@gmail.com>
2023-01-26 23:56:32 +0530

    Iterative DFS

--- dsa/leetcode/2359.find-closest-node-to-given-two-nodes.py
+++ dsa/leetcode/2359.find-closest-node-to-given-two-nodes.py
@@ -2,9 +2,42 @@
 from typing import List
 
 
+# https://leetcode.com/problems/find-closest-node-to-given-two-nodes/submissions/885828035/
 class Solution:
-    # https://leetcode.com/problems/find-closest-node-to-given-two-nodes/submissions/884775497/
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
+        a, b = node1, node2
+        path1, path2 = set(), set()
+
+        while a != -1 and b != -1:
+            if a in path1 or b in path2:
+                break
+            path1.add(a)
+            path2.add(b)
+            if b in path1:
+                if a in path2:
+                    return min(a, b)  # If there are multiple answers
+                return b
+            if a in path2:
+                return a
+            a = edges[a]
+            b = edges[b]
+
+        while a != -1 and a not in path1:
+            path1.add(a)
+            if a in path2:
+                return a
+            a = edges[a]
+
+        while b != -1 and b not in path2:
+            path2.add(b)
+            if b in path1:
+                return b
+            b = edges[b]
+
+        return -1
+
+    # https://leetcode.com/problems/find-closest-node-to-given-two-nodes/submissions/884775497/
+    def closestMeetingNode2(self, edges: List[int], node1: int, node2: int) -> int:
         path1, path2 = set(), set()
 
         # RecursionError: maximum recursion depth exceeded while calling a Python object
@@ -28,3 +61,12 @@ class Solution:
             return dfs(edges[u], edges[v])
 
         return dfs(node1, node2)
+
+
+if __name__ == "__main__":
+    testcases = [
+        [[1, 2, -1], 0, 2]
+    ]
+    for e, n1, n2 in testcases:
+        obj = Solution()
+        print(obj.closestMeetingNode2(e, n1, n2))
