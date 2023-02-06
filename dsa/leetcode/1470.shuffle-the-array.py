c68bcc04202fc19d4624c4e7b0511a03245cc580
yprakash <yprakash.518@gmail.com>
2023-02-06 21:49:58 +0530

    Single liner list comprehension two elements

--- dsa/leetcode/1470.shuffle-the-array.py
+++ dsa/leetcode/1470.shuffle-the-array.py
@@ -1,12 +1,19 @@
 # @author: yprakash
+from functools import reduce
+from typing import List
+
 
 class Solution:
-    # https://leetcode.com/submissions/detail/704885972/
+    # https://leetcode.com/problems/shuffle-the-array/submissions/892743939/
     def shuffle(self, nums: List[int], n: int) -> List[int]:
+        return [a for x, y in zip(nums, nums[n:]) for a in (x, y)]
+
+    # https://leetcode.com/submissions/detail/704885972/
+    def shuffle1(self, nums: List[int], n: int) -> List[int]:
         return reduce(lambda a, b: a + b, [[nums[i], nums[j]] for i, j in zip(range(0, n), range(n, 2 * n))])
 
     # https://leetcode.com/submissions/detail/704892639/
-    def shuffle(self, nums: List[int], n: int) -> List[int]:
+    def shuffle2(self, nums: List[int], n: int) -> List[int]:
         res = []
         for i in range(n):
             res.append(nums[i])
