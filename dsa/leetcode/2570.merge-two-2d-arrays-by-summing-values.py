a8668d80379d4fd5fcff5bc60df2da06abe1bed1
yprakash <yprakash.518@gmail.com>
2023-03-12 10:06:52 +0530

    Accepted easy in weekly-contest-333

---
+++ dsa/leetcode/2570.merge-two-2d-arrays-by-summing-values.py
@@ -0,0 +1,32 @@
+# @author: yprakash
+from typing import List
+
+
+# https://leetcode.com/problems/merge-two-2d-arrays-by-summing-values/submissions/900679194/
+class Solution:
+    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        res = []
+        i, j = 0, 0
+        m, n = len(nums1), len(nums2)
+
+        while i < m and j < n:
+            if nums1[i][0] < nums2[j][0]:
+                res.append(nums1[i])
+                i += 1
+            elif nums1[i][0] > nums2[j][0]:
+                res.append(nums2[j])
+                j += 1
+            else:
+                res.append([nums1[i][0], nums1[i][1] + nums2[j][1]])
+                i += 1
+                j += 1
+
+        while i < m:
+            res.append(nums1[i])
+            i += 1
+
+        while j < n:
+            res.append(nums2[j])
+            j += 1
+
+        return res
