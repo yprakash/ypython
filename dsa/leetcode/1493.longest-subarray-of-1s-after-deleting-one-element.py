5d8b8d0fffd275320d56c0662188ae11ed8397a8
yprakash <yprakash.518@gmail.com>
2023-07-06 00:28:16 +0530

    Sliding window and DP tabulation solutions

--- dsa/leetcode/1493.longest-subarray-of-1s-after-deleting-one-element.py
+++ dsa/leetcode/1493.longest-subarray-of-1s-after-deleting-one-element.py
@@ -5,7 +5,8 @@ from typing import List
 
 class Solution:
     # https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/submissions/986601357/
-    def longestSubarray(self, nums: List[int]) -> int:
+    # DP memoization
+    def longestSubarray1(self, nums: List[int]) -> int:
         @cache
         def longest(curr_index, removed_any_till_now, prev_max):
             if curr_index >= len(nums):
@@ -25,3 +26,38 @@ class Solution:
         if long == len(nums):
             return long - 1
         return long
+
+    # https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/submissions/987125836/
+    # DP Tabulation
+    def longestSubarray2(self, nums: List[int]) -> int:
+        ans = nums[0]
+        dp = [[0, 0] for i in range(len(nums))]
+        dp[0][0] = dp[0][1] = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] == 1:  #
+                dp[i][0] = 1 + dp[i-1][0]
+                dp[i][1] = 1 + dp[i-1][1]
+            else:
+                dp[i][0] = 0
+                dp[i][1] = 1 + dp[i-1][0]
+
+            ans = max(ans, dp[i][0], dp[i][1])
+
+        return ans - 1
+
+    # https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/submissions/987201799/
+    # Calculate consecutive 1's to left and right side of every 0 and take max
+    # Sliding Window technique
+    def longestSubarray(self, nums: List[int]) -> int:
+        left_sum, right_sum, best = 0, 0, 0
+        for n in nums:
+            if n:
+                right_sum += 1
+            else:
+                best = max(best, left_sum + right_sum)
+                left_sum = right_sum
+                right_sum = 0
+
+        best = max(best, left_sum + right_sum)
+        return best if any(n == 0 for n in nums) else best - 1
