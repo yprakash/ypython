5908743b717fa11cfc97559f43d42c93a65a09c9
yprakash <yprakash.518@gmail.com>
2023-07-31 19:48:20 +0530

    Learn this DP

---
+++ dsa/leetcode/712.minimum-ascii-delete-sum-for-two-strings.py
@@ -0,0 +1,20 @@
+# @author: yprakash
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        dp = [[-1] * (1 + len(s2)) for _ in range(1 + len(s1))]
+        return self.solve(s1, s2, 0, 0, dp)
+
+    def solve(self, s1, s2, i, j, dp):
+        if dp[i][j] > -1:
+            return dp[i][j]
+
+        if i == len(s1):
+            return sum(ord(ch) for ch in s2[j:])
+        if j == len(s2):
+            return sum(ord(ch) for ch in s1[i:])
+        nt = min(ord(s1[i]) + self.solve(s1, s2, i + 1, j, dp), ord(s2[j]) + self.solve(s1, s2, i, j + 1, dp))
+        tk = float('inf')
+        if s1[i] == s2[j]:
+            tk = self.solve(s1, s2, i + 1, j + 1, dp)
+        dp[i][j] = min(nt, tk)
+        return dp[i][j]
